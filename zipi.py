074d807573bca52610e1e611a6cb795ea5497413
FII14 <121723975+FII14@users.noreply.github.com>
2023-02-05 22:00:59 +0700

    Update zipi.py

--- zipi.py
+++ zipi.py
@@ -10,6 +10,7 @@ r=Fore.RESET
 h=Fore.LIGHTGREEN_EX
 k=Fore.LIGHTYELLOW_EX
 m=Fore.LIGHTRED_EX
+c=Fore.LIGHTCYAN_EX
 print(f"""{b}
 #####################################
 # d88888D d888888b d8888b. d888888b #
@@ -20,11 +21,11 @@ print(f"""{b}
 # d88888P Y888888P 88      Y888888P # 
 ############### FII14 ###############
 {r}""")
-file_zip = input("[»] Masukan nama file zip: ")
+file_zip = input(f"{c}[»] Masukan nama file zip: ")
 if(os.path.isfile(file_zip)):
     if file_zip.endswith(".zip"):
         time.sleep(1)
-        file_wordlist = input("[»] Masukan nama file wordlist: ")
+        file_wordlist = input(f"{c}[»] Masukan nama file wordlist: ")
         if(os.path.isfile(file_wordlist)):
             with zipfile.ZipFile(file_zip, "r") as fz:
                 with open(file_wordlist, "r") as fw:
@@ -33,13 +34,13 @@ if(os.path.isfile(file_zip)):
                         try:
                             fz.extractall(pwd=bytes(kata_sandi, "utf-8"))
                             time.sleep(1)
-                            print(f"[INFO] Kata sandi ditemukan: {kata_sandi}\n")
+                            print(f"{j}[INFO] Kata sandi ditemukan: {kata_sandi}{r}\n")
                             break
                         except:
                             time.sleep(1)
                             print(f"{k}[!] Mencoba kata sandi: {kata_sandi}{r}")
                     else:
-                        print("[INFO] Kata sandi tidak ditemukan dalam file wordlist.\n")
+                        print(f"{m}[INFO] Kata sandi tidak ditemukan dalam file wordlist.{r}\n")
         else:
             time.sleep(3)
             print(f"[INFO] File {wordlist_file} tidak ditemukan.\n")
