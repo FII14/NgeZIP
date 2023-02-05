4486c6dcecca0de0cae2df62c88e46868b9b5238
FII14 <121723975+FII14@users.noreply.github.com>
2023-02-05 19:52:39 +0700

    Update zipi.py

--- zipi.py
+++ zipi.py
@@ -4,26 +4,25 @@ import os.path
 
 file_zip = input("Masukan nama file zip: ")
 
-if(os.path.isfile(file_zip) & file_zip.endswith(".zip")):
-    file_wordlist = input("Masukan nama file wordlist: ")
-
-    if(os.path.isfile(file_wordlist)):
-        
-        with zipfile.ZipFile(file_zip, "r") as fz:
-            with open(file_wordlist, "r") as fw:
-                for baris in fw:
-                    kata_sandi = baris.strip()
-                    try:
-                        fz.extractall(pwd=bytes(kata_sandi, "utf-8"))
-                        print(f"Kata sandi ditemukan: {kata_sandi}")
-                        break
-                    except:
-                        print(f"Mencoba kata sandi: {kata_sandi}")
-                else:
-                    print("Kata sandi tidak ditemukan dalam file wordlist.")
-
+if(os.path.isfile(file_zip):
+    if file_zip.endswith(".zip"):
+        file_wordlist = input("Masukan nama file wordlist: ")
+        if(os.path.isfile(file_wordlist)):
+            with zipfile.ZipFile(file_zip, "r") as fz:
+                with open(file_wordlist, "r") as fw:
+                    for baris in fw:
+                        kata_sandi = baris.strip()
+                        try:
+                            fz.extractall(pwd=bytes(kata_sandi, "utf-8"))
+                            print(f"Kata sandi ditemukan: {kata_sandi}")
+                            break
+                        except:
+                            print(f"Mencoba kata sandi: {kata_sandi}")
+                    else:
+                        print("Kata sandi tidak ditemukan dalam file wordlist.")
+        else:
+            print(f"File {wordlist_file} tidak ditemukan.")
     else:
-        print(f"File {wordlist_file} tidak ditemukan.")
-
+        print(f"File {file_zip} bukan file zip.")
 else:
     print(f"File {file_zip} tidak ditemukan.")
