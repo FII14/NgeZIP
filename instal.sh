f9e286ea737aa5184343a07e64db92f13179223f
FII14 <121723975+FII14@users.noreply.github.com>
2023-02-09 21:58:48 +0700

    Update instal.sh

--- instal.sh
+++ instal.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 # File untuk menginstal ZIPI otomatis.
 
-apt-get update -y
 apt-get install python3-pip -y
-apt-get install git -y
-git clone https://github.com/FII14/ZIPI
-cd ZIPI
 pip3 install colorama
 python3 zipi.py
