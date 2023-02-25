36a541f701a91102e8fee88a7a99d38e2c3a726d
FII14 <121723975+FII14@users.noreply.github.com>
2023-02-25 18:19:22 +0700

    Update pasang.sh

--- pasang.sh
+++ pasang.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # File untuk mendeskripsi file ngezip
 
+apt-get update
+apt-get install python3-pip
+pip3 install colorama
 base64 -d src/ngezip > src/ngezip.py
 rm src/ngezip
 chmod +x src/ngezip.py
